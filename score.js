758f879f36196fa176f4c8cde92113c75a8aca11
Narendra Chatterjee <48941364+narendrachatterjee@users.noreply.github.com>
2024-03-06 18:56:38 +0530

    Update score.js

--- score.js
+++ score.js
@@ -1,1 +1,15 @@
 // Iteration 5: Store the player score and display it on the game over screen
+const scoreBox = document.getElementById("score-board");
+let passcode = localStorage.getItem("squad56");
+
+console.log(passcode)
+
+
+scoreBox.innerHTML = passcode;
+
+
+const playAgainButton = document.getElementById("play-again-button")
+
+playAgainButton.onclick =()=>{
+    location.href="./game.html"
+}
